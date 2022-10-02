86c0a2055ab544d74ecc1d659e593ca2447fbebc
Rasul <rasul.ceo@gmail.com>
2022-10-02 18:53:13 +0300

    word search bar added

--- components/wordList/wordList.js
+++ components/wordList/wordList.js
@@ -3,6 +3,7 @@ import { Word } from "../word/word";
 import { FlatList, View, TextInput } from "react-native";
 import { CustomText } from "../custom/CustomText";
 import { Context } from "../../navigation";
+import { COLORS } from "../../colors";
 
 export const WordList = ({ data, navigation }) => {
   const { dynamicColor } = React.useContext(Context);
@@ -27,50 +28,111 @@ export const WordList = ({ data, navigation }) => {
         ]}
       >
         <TextInput
-          placeholder="Найти слово"
+          placeholder="Найти слово (введите любой его эквивалент)"
           value={searchWord}
           onChangeText={setSearchWord}
+          style={{
+            borderBottomWidth: 1,
+            borderBottomColor: COLORS.DARK_GRAY,
+            marginBottom: 12,
+          }}
         />
-        <CustomText
-          title={`Слов в категории: ${data.length}`}
+
+        {/* <CustomText
+          title={`Слов в категории: ${
+            searchWord ? filteredBySearch.length : data.length
+          }`}
           color={dynamicColor}
-        />
-      </View>
-      
-      <FlatList
-        data={data.sort((firstByAlphabet, secondByAlphabet) =>
-          firstByAlphabet.khadar.localeCompare(secondByAlphabet.khadar)
-        )}
-        keyExtractor={(item) => item.src}
-        renderItem={({ item, index }) => (
-          <Word
-            id={item.id}
-            index={index + 1}
-            src={item.src}
-            russian={item.russian}
-            khadar={item.khadar}
-            english={item.english}
-            additional_info={item.additional_info}
-            sound_src={item.sound_src}
-            navigation={navigation}
-            length={data.length}
-            onPress={() =>
-              navigation.navigate("Selected Word", {
-                id: item.id,
-                index: index + 1,
-                src: item.src,
-                category: item.category,
-                russian: item.russian,
-                khadar: item.khadar,
-                english: item.english,
-                additional_info: item.additional_info,
-                sound_src: item.sound_src,
-                length: data.length,
-              })
-            }
+        /> */}
+        {searchWord && !filteredBySearch.length ? (
+          <View>
+            <CustomText
+              title="Нет совпадений."
+              color={dynamicColor}
+              fontWeight="700"
+              textAlign="center"
+            />
+          </View>
+        ) : (
+          <CustomText
+            title={`Слов в категории: ${
+              searchWord ? filteredBySearch.length : data.length
+            }`}
+            color={dynamicColor}
           />
         )}
-      />
+      </View>
+      {searchWord ? (
+        <FlatList
+          data={filteredBySearch.sort((firstByAlphabet, secondByAlphabet) =>
+            firstByAlphabet.khadar.localeCompare(secondByAlphabet.khadar)
+          )}
+          keyExtractor={(item) => item.src}
+          renderItem={({ item, index }) => (
+            <Word
+              id={item.id}
+              index={index + 1}
+              src={item.src}
+              russian={item.russian}
+              khadar={item.khadar}
+              english={item.english}
+              additional_info={item.additional_info}
+              sound_src={item.sound_src}
+              navigation={navigation}
+              length={data.length}
+              onPress={() =>
+                navigation.navigate("Selected Word", {
+                  id: item.id,
+                  index: index + 1,
+                  src: item.src,
+                  category: item.category,
+                  russian: item.russian,
+                  khadar: item.khadar,
+                  english: item.english,
+                  additional_info: item.additional_info,
+                  sound_src: item.sound_src,
+                  length: data.length,
+                })
+              }
+            />
+          )}
+        />
+      ) : (
+        <FlatList
+          data={data.sort((firstByAlphabet, secondByAlphabet) =>
+            firstByAlphabet.khadar.localeCompare(secondByAlphabet.khadar)
+          )}
+          keyExtractor={(item) => item.src}
+          renderItem={({ item, index }) => (
+            <Word
+              id={item.id}
+              index={index + 1}
+              src={item.src}
+              russian={item.russian}
+              khadar={item.khadar}
+              english={item.english}
+              additional_info={item.additional_info}
+              sound_src={item.sound_src}
+              navigation={navigation}
+              length={data.length}
+              onPress={() =>
+                navigation.navigate("Selected Word", {
+                  id: item.id,
+                  index: index + 1,
+                  src: item.src,
+                  category: item.category,
+                  russian: item.russian,
+                  khadar: item.khadar,
+                  english: item.english,
+                  additional_info: item.additional_info,
+                  sound_src: item.sound_src,
+                  length: data.length,
+                })
+              }
+            />
+          )}
+        />
+      )}
     </>
   );
 };
