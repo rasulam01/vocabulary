991a1f9ea7780a13d4dc60342365a31cdf3a36a5
Rasul <rasul.ceo@gmail.com>
2022-10-16 17:19:41 +0300

    rules part added

--- components/listData/partsList.js
+++ components/listData/partsList.js
@@ -1,12 +1,10 @@
 export const partsList = [
-  // {
-  //   src: require("../../assets/sections/rules.png"),
-  //   title: "Правила",
-  //   title: "Subsections",
-  // },
   {
-    src: require("../../assets/sections/dictionary.png"),
-    title: "Словарь",
+    src: require("../../assets/sections/rules.png"),    
+    title: "rules",
+  },
+  {
+    src: require("../../assets/sections/dictionary.png"),    
     title: "speech_parts",
   },
 ];
