f030379f60c939fb67227f2a7feab682b64a49ba
Rasul <rasul.ceo@gmail.com>
2023-04-24 10:50:16 +0300

    proguard

--- App.js
+++ App.js
@@ -8,22 +8,22 @@
 
 import React, { useEffect } from "react";
 import Navigation from "./navigation";
-import {
-  MobileAds,
-  InterstitialAdManager,
-} from "react-native-yandex-mobile-ads";
-import SplashScreen from "react-native-splash-screen";
+// import {
+//   MobileAds,
+//   InterstitialAdManager,
+// } from "react-native-yandex-mobile-ads";
+// import SplashScreen from "react-native-splash-screen";
 
-MobileAds.initialize({ userConsent: true, locationConsent: true });
+// MobileAds.initialize({ userConsent: true, locationConsent: true });
 
 const App = () => {
   useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
     }, 2000);
-    setTimeout(() => {
-      InterstitialAdManager.showAd("R-M-1959796-1")
-    }, 2000);
+    // setTimeout(() => {
+    //   InterstitialAdManager.showAd("R-M-1959796-1")
+    // }, 2000);
   });
 
   return <Navigation />;
